4c9248685118fa284598766ca458c077b8a4b735
rupamaheswari <rupamaheswari97@gmail.com>
2017-09-13 12:17:59 +0530

    Add files via upload

---
+++ src/exam/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args)
+	{
+        Student[ ] students;
+	    int index;
+	    Scanner s=new Scanner(System.in);
+	    index=s.nextInt();
+	    StudentGroup st= new StudentGroup(10);
+	    st.getStudent(index);
+	    System.out.println(Students[index]);
+
+
+
+		//You may test that your code works find here
+		//Please check that your code works and has		//compilation problems before to submit
+	}
+
+}
